8a35b33496ef7c0cb0b74a24c8f52d747160567f
Longshot900 <longshot900@gmail.com>
2014-06-17 12:17:04 +0800

    The MVC Project of the Rent-a-car Project
    This is a folder of tall the MVC Work that has been done for Rent-A-Car

---
+++ MVC Rent-A-Car Website/RentACar/RentACar/Models/CarModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentACar.Models
+{
+    public class CarModel
+    {
+        public int ID { get; set; }
+        public string manufacturer { get; set; }
+        public string make { get; set; }
+        [Display(Name = "year")]
+        [DataType(DataType.Date)]
+        public DateTime year { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy}", ApplyFormatInEditMode = true)]
+        public double price { get; set; }
+    }
+
+    public class CarDBContext : DbContext
+    {
+        public DbSet<CarModel> Cars { get; set; }
+    }
+}
